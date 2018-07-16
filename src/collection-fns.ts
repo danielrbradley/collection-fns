4c8f21d3901c3f3b44ac282d995d99971acc3b93
Daniel Bradley <daniel@danielbradley.net>
2018-07-16 15:13:42 +0100

    Implement initial arrays module
    Mirror iterables implementation

--- src/collection-fns.ts
+++ src/collection-fns.ts
@@ -1,6 +1,8 @@
 import * as iterables from './iterables'
+import * as arrays from './arrays'
 import * as maps from './maps'
 export * from './pipes'
 
 export const Iterables = iterables
+export const Arrays = arrays
 export const Maps = maps
