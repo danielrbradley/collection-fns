8b766911a17ce8d21df80a8563afeda2f21cefe0
Daniel Bradley <daniel@danielbradley.net>
2018-07-17 10:12:55 +0100

    feat(Sets): Add sets module
    Create initial functions for the sets module

---
+++ src/sets.ts
@@ -0,0 +1,116 @@
+import { Iterables } from './collection-fns'
+
+export function ofIterable<T>(source: Iterable<T>): Set<T> {
+  return new Set(source)
+}
+
+export function asIterable<T>(source: Set<T>): Iterable<T> {
+  return source
+}
+
+export function map<T, U>(mapping: (item: T) => U): (source: Set<T>) => Set<U>
+export function map<T, U>(source: Set<T>, mapping: (item: T) => U): Set<U>
+export function map<T, U>(a: any, b?: any): any {
+  const partial = typeof a === 'function'
+  const mapping: (item: T) => U = partial ? a : b
+  function exec(source: Set<T>) {
+    return new Set(Iterables.map(source.values(), mapping))
+  }
+  return partial ? exec : exec(a)
+}
+
+export function filter<T>(predicate: (item: T) => boolean): (source: Set<T>) => Set<T>
+export function filter<T>(source: Set<T>, predicate: (item: T) => boolean): Set<T>
+export function filter<T>(a: any, b?: any): any {
+  const partial = typeof a === 'function'
+  const predicate: (item: T) => boolean = partial ? a : b
+  function exec(source: Set<T>) {
+    return new Set(Iterables.filter(source.values(), predicate))
+  }
+  return partial ? exec : exec(a)
+}
+
+export function choose<T, U>(chooser: (item: T) => U | undefined): (source: Set<T>) => Set<U>
+export function choose<T, U>(source: Set<T>, chooser: (item: T) => U | undefined): Set<U>
+export function choose<T, U>(a: any, b?: any): any {
+  const partial = typeof a === 'function'
+  const chooser: (item: T) => U | undefined = partial ? a : b
+  function exec(source: Set<T>) {
+    return new Set<U>(Iterables.choose(source.values(), chooser))
+  }
+  return partial ? exec : exec(a)
+}
+
+export function collect<T, U>(mapping: (item: T) => Iterable<U>): (source: Set<T>) => Set<U>
+export function collect<T, U>(source: Set<T>, mapping: (item: T) => Iterable<U>): Set<U>
+export function collect<T, U>(a: any, b?: any): any {
+  const partial = typeof a === 'function'
+  const mapping: (item: T) => Iterable<U> = partial ? a : b
+  function exec(source: Set<T>) {
+    return new Set<U>(Iterables.collect(source, mapping))
+  }
+  return partial ? exec : exec(a)
+}
+
+export function append<T>(second: Set<T>): (first: Set<T>) => Set<T>
+export function append<T>(first: Set<T>, second: Set<T>): Set<T>
+export function append<T>(a: any, b?: any): any {
+  const partial = b === undefined
+  const second: Set<T> = partial ? a : b
+  function exec(first: Set<T>): Set<T> {
+    return new Set<T>(Iterables.append(first, second))
+  }
+  return partial ? exec : exec(a)
+}
+
+export function concat<T>(sources: Iterable<Set<T>>): Set<T> {
+  const target = new Set<T>()
+  for (const source of sources) {
+    for (const item of source) {
+      target.add(item)
+    }
+  }
+  return target
+}
+
+export function exists<T>(predicate: (item: T) => boolean): (source: Set<T>) => boolean
+export function exists<T>(source: Set<T>, predicate: (item: T) => boolean): boolean
+export function exists<T>(a: any, b?: any): any {
+  const partial = typeof a === 'function'
+  const predicate: (item: T) => boolean = partial ? a : b
+  function exec(source: Set<T>): boolean {
+    return Iterables.exists(source, predicate)
+  }
+  return partial ? exec : exec(a)
+}
+
+export function contains<T>(item: T): (source: Set<T>) => boolean
+export function contains<T>(source: Set<T>, item: T): boolean
+export function contains<T>(a: any, b?: any): any {
+  const partial = b === undefined
+  const item: T = partial ? a : b
+  function exec(source: Set<T>): boolean {
+    return source.has(item)
+  }
+  return partial ? exec : exec(a)
+}
+
+export function find<T>(predicate: (item: T) => boolean): (source: Set<T>) => T | undefined
+export function find<T>(source: Set<T>, predicate: (item: T) => boolean): T | undefined
+export function find<T>(a: any, b?: any): any {
+  const partial = typeof a === 'function'
+  const predicate: (item: T) => boolean = partial ? a : b
+  function exec(source: Set<T>): T | undefined {
+    for (const item of source) {
+      if (predicate(item)) {
+        return item
+      }
+    }
+    return undefined
+  }
+  return partial ? exec : exec(a)
+}
+
+export function count<T>(source: Set<T>): number {
+  return source.size
+}
---
+++ test/sets.test.ts
@@ -0,0 +1,140 @@
+import { pipe, Sets, Iterables } from '../src/collection-fns'
+
+describe('ofIterable', () => {
+  it('constructs an array', () => {
+    const iterator = function*() {
+      yield 1
+      yield 2
+      yield 2
+    }
+    expect(Sets.ofIterable(iterator())).toEqual(new Set([1, 2]))
+  })
+})
+
+test('asIterable', () => {
+  expect(Iterables.length(Sets.asIterable(new Set(['a'])))).toEqual(1)
+})
+
+describe('map', () => {
+  test('empty', () => {
+    expect(Sets.map(x => '')(new Set())).toEqual(new Set())
+  })
+  test('piped', () => {
+    expect(pipe(new Set([1, 2, 3])).then(Sets.map(x => x % 2)).result).toEqual(new Set([0, 1]))
+  })
+  test('invoke', () => {
+    expect(Sets.map(new Set([1, 2, 3]), x => x % 2)).toEqual(new Set([0, 1]))
+  })
+})
+
+describe('filter', () => {
+  test('empty', () => {
+    expect(Sets.filter(new Set(), x => true)).toEqual(new Set([]))
+  })
+  test('piped', () => {
+    expect(pipe(new Set([1, 2, 3, 4])).then(Sets.filter(x => x % 2 === 0)).result).toEqual(
+      new Set([2, 4])
+    )
+  })
+  test('invoke', () => {
+    expect(Sets.filter(new Set([1, 2, 3, 4]), x => x % 2 === 0)).toEqual(new Set([2, 4]))
+  })
+})
+
+describe('choose', () => {
+  test('piped', () => {
+    expect(
+      pipe(new Set([1, 2, 3])).then(Sets.choose(x => (x % 2 === 1 ? x * 2 : undefined))).result
+    ).toEqual(new Set([2, 6]))
+  })
+  test('invoke', () => {
+    expect(Sets.choose(new Set([1, 2, 3]), x => (x % 2 === 1 ? x * 2 : undefined))).toEqual(
+      new Set([2, 6])
+    )
+  })
+})
+
+describe('collect', () => {
+  test('piped', () => {
+    expect(
+      pipe(new Set([1, 2])).then(
+        Sets.collect(function(x) {
+          return [x, x + 1]
+        })
+      ).result
+    ).toEqual(new Set([1, 2, 3]))
+  })
+  test('invoke', () => {
+    expect(
+      Sets.collect(new Set([1, 2]), function(x) {
+        return [x, x + 1]
+      })
+    ).toEqual(new Set([1, 2, 3]))
+  })
+})
+
+describe('append', () => {
+  test('piped', () => {
+    expect(pipe(new Set([1, 2])).then(Sets.append(new Set([2, 3]))).result).toEqual(
+      new Set([1, 2, 3])
+    )
+  })
+  test('invoke', () => {
+    expect(Sets.append(new Set([1, 2]), new Set([2, 3]))).toEqual(new Set([1, 2, 3]))
+  })
+})
+
+test('concat', () => {
+  expect(Sets.concat([new Set([1, 2]), new Set([2, 3, 4]), new Set([])])).toEqual(
+    new Set([1, 2, 3, 4])
+  )
+})
+
+describe('exists', () => {
+  it('matches existance', () => {
+    expect(pipe(new Set([1, 2])).then(Sets.exists(x => x % 2 === 1)).result).toEqual(true)
+  })
+  it('matches non-existance', () => {
+    expect(pipe(new Set([1, 2])).then(Sets.exists(x => x % 3 === 0)).result).toEqual(false)
+  })
+  test('invoke', () => {
+    expect(Sets.exists(new Set([1, 2]), x => x % 2 === 1)).toEqual(true)
+  })
+})
+
+describe('contains', () => {
+  test('piped match', () => {
+    expect(pipe(new Set(['amy', 'bob'])).then(Sets.contains('bob')).result).toEqual(true)
+  })
+  test('no match', () => {
+    expect(pipe(new Set(['amy', 'bob'])).then(Sets.contains('cat')).result).toEqual(false)
+  })
+  test('invoke', () => {
+    expect(Sets.contains(new Set(['amy', 'bob']), 'amy')).toEqual(true)
+  })
+})
+
+describe('find', () => {
+  test('piped match', () => {
+    expect(pipe(new Set(['amy', 'bob'])).then(Sets.find(x => x.startsWith('b'))).result).toEqual(
+      'bob'
+    )
+  })
+  it('returns undefined when not found', () => {
+    expect(
+      pipe(new Set(['amy', 'bob'])).then(Sets.find(x => x.startsWith('c'))).result
+    ).toBeUndefined()
+  })
+  test('invoke', () => {
+    expect(Sets.find(new Set(['amy', 'bob']), x => x.startsWith('a'))).toEqual('amy')
+  })
+})
+
+describe('count', () => {
+  test('zero length', () => {
+    expect(Sets.count(new Set())).toEqual(0)
+  })
+  test('non-zero length', () => {
+    expect(Sets.count(new Set([1, 2, 3, 4, 5]))).toEqual(5)
+  })
+})
