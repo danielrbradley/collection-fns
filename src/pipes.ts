58cc49b216a79475b10c979198496f3ed9aff1ca
Daniel Bradley <daniel@danielbradley.net>
2018-07-19 22:10:13 +0100

    feat(pipe): Add pipe overloads
    Add multi-argument overloads for pipe where the next argument is a function taking the result of the last argument and returns a potentially new type. These overloads return the result immediately at the end of the function instead of creating a pipe object.

--- src/pipes.ts
+++ src/pipes.ts
@@ -8,6 +8,403 @@ export class Pipe<T> {
   }
 }
 
-export function pipe<T>(input: T) {
-  return new Pipe(input)
+export function pipe<T>(input: T): Pipe<T>
+export function pipe<A, B>(a: A, b: (value: A) => B): B
+export function pipe<A, B, C>(a: A, b: (value: A) => B, c: (value: B) => C): C
+export function pipe<A, B, C, D>(
+  a: A,
+  b: (value: A) => B,
+  c: (value: B) => C,
+  d: (value: C) => D
+): D
+export function pipe<A, B, C, D, E>(
+  a: A,
+  b: (value: A) => B,
+  c: (value: B) => C,
+  d: (value: C) => D,
+  e: (value: D) => E
+): E
+export function pipe<A, B, C, D, E, F>(
+  a: A,
+  b: (value: A) => B,
+  c: (value: B) => C,
+  d: (value: C) => D,
+  e: (value: D) => E,
+  f: (value: E) => F
+): F
+export function pipe<A, B, C, D, E, F, G>(
+  a: A,
+  b: (value: A) => B,
+  c: (value: B) => C,
+  d: (value: C) => D,
+  e: (value: D) => E,
+  f: (value: E) => F,
+  g: (value: F) => G
+): G
+export function pipe<A, B, C, D, E, F, G, H>(
+  a: A,
+  b: (value: A) => B,
+  c: (value: B) => C,
+  d: (value: C) => D,
+  e: (value: D) => E,
+  f: (value: E) => F,
+  g: (value: F) => G,
+  h: (value: G) => H
+): H
+export function pipe<A, B, C, D, E, F, G, H, I>(
+  a: A,
+  b: (value: A) => B,
+  c: (value: B) => C,
+  d: (value: C) => D,
+  e: (value: D) => E,
+  f: (value: E) => F,
+  g: (value: F) => G,
+  h: (value: G) => H,
+  i: (value: H) => I
+): I
+export function pipe<A, B, C, D, E, F, G, H, I, J>(
+  a: A,
+  b: (value: A) => B,
+  c: (value: B) => C,
+  d: (value: C) => D,
+  e: (value: D) => E,
+  f: (value: E) => F,
+  g: (value: F) => G,
+  h: (value: G) => H,
+  i: (value: H) => I,
+  j: (value: I) => J
+): J
+export function pipe<A, B, C, D, E, F, G, H, I, J, K>(
+  a: A,
+  b: (value: A) => B,
+  c: (value: B) => C,
+  d: (value: C) => D,
+  e: (value: D) => E,
+  f: (value: E) => F,
+  g: (value: F) => G,
+  h: (value: G) => H,
+  i: (value: H) => I,
+  j: (value: I) => J,
+  k: (value: J) => K
+): K
+export function pipe<A, B, C, D, E, F, G, H, I, J, K, L>(
+  a: A,
+  b: (value: A) => B,
+  c: (value: B) => C,
+  d: (value: C) => D,
+  e: (value: D) => E,
+  f: (value: E) => F,
+  g: (value: F) => G,
+  h: (value: G) => H,
+  i: (value: H) => I,
+  j: (value: I) => J,
+  k: (value: J) => K,
+  l: (value: K) => L
+): L
+export function pipe<A, B, C, D, E, F, G, H, I, J, K, L, M>(
+  a: A,
+  b: (value: A) => B,
+  c: (value: B) => C,
+  d: (value: C) => D,
+  e: (value: D) => E,
+  f: (value: E) => F,
+  g: (value: F) => G,
+  h: (value: G) => H,
+  i: (value: H) => I,
+  j: (value: I) => J,
+  k: (value: J) => K,
+  l: (value: K) => L,
+  m: (value: L) => M
+): M
+export function pipe<A, B, C, D, E, F, G, H, I, J, K, L, M, N>(
+  a: A,
+  b: (value: A) => B,
+  c: (value: B) => C,
+  d: (value: C) => D,
+  e: (value: D) => E,
+  f: (value: E) => F,
+  g: (value: F) => G,
+  h: (value: G) => H,
+  i: (value: H) => I,
+  j: (value: I) => J,
+  k: (value: J) => K,
+  l: (value: K) => L,
+  m: (value: L) => M,
+  n: (value: M) => N
+): N
+export function pipe<A, B, C, D, E, F, G, H, I, J, K, L, M, N, O>(
+  a: A,
+  b: (value: A) => B,
+  c: (value: B) => C,
+  d: (value: C) => D,
+  e: (value: D) => E,
+  f: (value: E) => F,
+  g: (value: F) => G,
+  h: (value: G) => H,
+  i: (value: H) => I,
+  j: (value: I) => J,
+  k: (value: J) => K,
+  l: (value: K) => L,
+  m: (value: L) => M,
+  n: (value: M) => N,
+  o: (value: N) => O
+): O
+export function pipe<A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P>(
+  a: A,
+  b: (value: A) => B,
+  c: (value: B) => C,
+  d: (value: C) => D,
+  e: (value: D) => E,
+  f: (value: E) => F,
+  g: (value: F) => G,
+  h: (value: G) => H,
+  i: (value: H) => I,
+  j: (value: I) => J,
+  k: (value: J) => K,
+  l: (value: K) => L,
+  m: (value: L) => M,
+  n: (value: M) => N,
+  o: (value: N) => O,
+  p: (value: O) => P
+): P
+export function pipe<A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q>(
+  a: A,
+  b: (value: A) => B,
+  c: (value: B) => C,
+  d: (value: C) => D,
+  e: (value: D) => E,
+  f: (value: E) => F,
+  g: (value: F) => G,
+  h: (value: G) => H,
+  i: (value: H) => I,
+  j: (value: I) => J,
+  k: (value: J) => K,
+  l: (value: K) => L,
+  m: (value: L) => M,
+  n: (value: M) => N,
+  o: (value: N) => O,
+  p: (value: O) => P,
+  q: (value: P) => Q
+): Q
+export function pipe<A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R>(
+  a: A,
+  b: (value: A) => B,
+  c: (value: B) => C,
+  d: (value: C) => D,
+  e: (value: D) => E,
+  f: (value: E) => F,
+  g: (value: F) => G,
+  h: (value: G) => H,
+  i: (value: H) => I,
+  j: (value: I) => J,
+  k: (value: J) => K,
+  l: (value: K) => L,
+  m: (value: L) => M,
+  n: (value: M) => N,
+  o: (value: N) => O,
+  p: (value: O) => P,
+  q: (value: P) => Q,
+  r: (value: Q) => R
+): R
+export function pipe<A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S>(
+  a: A,
+  b: (value: A) => B,
+  c: (value: B) => C,
+  d: (value: C) => D,
+  e: (value: D) => E,
+  f: (value: E) => F,
+  g: (value: F) => G,
+  h: (value: G) => H,
+  i: (value: H) => I,
+  j: (value: I) => J,
+  k: (value: J) => K,
+  l: (value: K) => L,
+  m: (value: L) => M,
+  n: (value: M) => N,
+  o: (value: N) => O,
+  p: (value: O) => P,
+  q: (value: P) => Q,
+  r: (value: Q) => R,
+  s: (value: R) => S
+): S
+export function pipe<A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T>(
+  a: A,
+  b: (value: A) => B,
+  c: (value: B) => C,
+  d: (value: C) => D,
+  e: (value: D) => E,
+  f: (value: E) => F,
+  g: (value: F) => G,
+  h: (value: G) => H,
+  i: (value: H) => I,
+  j: (value: I) => J,
+  k: (value: J) => K,
+  l: (value: K) => L,
+  m: (value: L) => M,
+  n: (value: M) => N,
+  o: (value: N) => O,
+  p: (value: O) => P,
+  q: (value: P) => Q,
+  r: (value: Q) => R,
+  s: (value: R) => S,
+  t: (value: S) => T
+): T
+export function pipe<A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U>(
+  a: A,
+  b: (value: A) => B,
+  c: (value: B) => C,
+  d: (value: C) => D,
+  e: (value: D) => E,
+  f: (value: E) => F,
+  g: (value: F) => G,
+  h: (value: G) => H,
+  i: (value: H) => I,
+  j: (value: I) => J,
+  k: (value: J) => K,
+  l: (value: K) => L,
+  m: (value: L) => M,
+  n: (value: M) => N,
+  o: (value: N) => O,
+  p: (value: O) => P,
+  q: (value: P) => Q,
+  r: (value: Q) => R,
+  s: (value: R) => S,
+  t: (value: S) => T,
+  u: (value: T) => U
+): U
+export function pipe<A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V>(
+  a: A,
+  b: (value: A) => B,
+  c: (value: B) => C,
+  d: (value: C) => D,
+  e: (value: D) => E,
+  f: (value: E) => F,
+  g: (value: F) => G,
+  h: (value: G) => H,
+  i: (value: H) => I,
+  j: (value: I) => J,
+  k: (value: J) => K,
+  l: (value: K) => L,
+  m: (value: L) => M,
+  n: (value: M) => N,
+  o: (value: N) => O,
+  p: (value: O) => P,
+  q: (value: P) => Q,
+  r: (value: Q) => R,
+  s: (value: R) => S,
+  t: (value: S) => T,
+  u: (value: T) => U,
+  v: (value: U) => V
+): V
+export function pipe<A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W>(
+  a: A,
+  b: (value: A) => B,
+  c: (value: B) => C,
+  d: (value: C) => D,
+  e: (value: D) => E,
+  f: (value: E) => F,
+  g: (value: F) => G,
+  h: (value: G) => H,
+  i: (value: H) => I,
+  j: (value: I) => J,
+  k: (value: J) => K,
+  l: (value: K) => L,
+  m: (value: L) => M,
+  n: (value: M) => N,
+  o: (value: N) => O,
+  p: (value: O) => P,
+  q: (value: P) => Q,
+  r: (value: Q) => R,
+  s: (value: R) => S,
+  t: (value: S) => T,
+  u: (value: T) => U,
+  v: (value: U) => V,
+  w: (value: V) => W
+): W
+export function pipe<A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X>(
+  a: A,
+  b: (value: A) => B,
+  c: (value: B) => C,
+  d: (value: C) => D,
+  e: (value: D) => E,
+  f: (value: E) => F,
+  g: (value: F) => G,
+  h: (value: G) => H,
+  i: (value: H) => I,
+  j: (value: I) => J,
+  k: (value: J) => K,
+  l: (value: K) => L,
+  m: (value: L) => M,
+  n: (value: M) => N,
+  o: (value: N) => O,
+  p: (value: O) => P,
+  q: (value: P) => Q,
+  r: (value: Q) => R,
+  s: (value: R) => S,
+  t: (value: S) => T,
+  u: (value: T) => U,
+  v: (value: U) => V,
+  w: (value: V) => W,
+  x: (value: W) => X
+): X
+export function pipe<A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y>(
+  a: A,
+  b: (value: A) => B,
+  c: (value: B) => C,
+  d: (value: C) => D,
+  e: (value: D) => E,
+  f: (value: E) => F,
+  g: (value: F) => G,
+  h: (value: G) => H,
+  i: (value: H) => I,
+  j: (value: I) => J,
+  k: (value: J) => K,
+  l: (value: K) => L,
+  m: (value: L) => M,
+  n: (value: M) => N,
+  o: (value: N) => O,
+  p: (value: O) => P,
+  q: (value: P) => Q,
+  r: (value: Q) => R,
+  s: (value: R) => S,
+  t: (value: S) => T,
+  u: (value: T) => U,
+  v: (value: U) => V,
+  w: (value: V) => W,
+  x: (value: W) => X,
+  y: (value: X) => Y
+): Y
+export function pipe<A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z>(
+  a: A,
+  b: (value: A) => B,
+  c: (value: B) => C,
+  d: (value: C) => D,
+  e: (value: D) => E,
+  f: (value: E) => F,
+  g: (value: F) => G,
+  h: (value: G) => H,
+  i: (value: H) => I,
+  j: (value: I) => J,
+  k: (value: J) => K,
+  l: (value: K) => L,
+  m: (value: L) => M,
+  n: (value: M) => N,
+  o: (value: N) => O,
+  p: (value: O) => P,
+  q: (value: P) => Q,
+  r: (value: Q) => R,
+  s: (value: R) => S,
+  t: (value: S) => T,
+  u: (value: T) => U,
+  v: (value: U) => V,
+  w: (value: V) => W,
+  x: (value: W) => X,
+  y: (value: X) => Y,
+  z: (value: Y) => Z
+): Z
+export function pipe(...args: any[]): any {
+  if (args.length === 1) {
+    return new Pipe(args[0])
+  }
+  return args.slice(1).reduce((last, next) => next(last), args[0])
 }
--- test/pipe.test.ts
+++ test/pipe.test.ts
@@ -1,7 +1,47 @@
 import { pipe } from '../src/collection-fns'
 
 describe('pipes', () => {
-  it('can pipe simple values', () => {
+  test('pipe object', () => {
     expect(pipe(1).then(x => x + 1).result).toEqual(2)
+  })
+  test('smallest immediate return', () => {
+    expect(
+      pipe(
+        1,
+        x => x + 1
+      )
+    ).toEqual(2)
+  })
+  test('longest', () => {
+    expect(
+      pipe(
+        { a: 1 },
+        x => ({ b: x.a + 1 }),
+        x => ({ c: x.b + 1 }),
+        x => ({ d: x.c + 1 }),
+        x => ({ e: x.d + 1 }),
+        x => ({ f: x.e + 1 }),
+        x => ({ g: x.f + 1 }),
+        x => ({ h: x.g + 1 }),
+        x => ({ i: x.h + 1 }),
+        x => ({ j: x.i + 1 }),
+        x => ({ k: x.j + 1 }),
+        x => ({ l: x.k + 1 }),
+        x => ({ m: x.l + 1 }),
+        x => ({ n: x.m + 1 }),
+        x => ({ o: x.n + 1 }),
+        x => ({ p: x.o + 1 }),
+        x => ({ q: x.p + 1 }),
+        x => ({ r: x.q + 1 }),
+        x => ({ s: x.r + 1 }),
+        x => ({ t: x.s + 1 }),
+        x => ({ u: x.t + 1 }),
+        x => ({ v: x.u + 1 }),
+        x => ({ w: x.v + 1 }),
+        x => ({ x: x.w + 1 }),
+        x => ({ y: x.x + 1 }),
+        x => ({ z: x.y + 1 })
+      )
+    ).toEqual({ z: 26 })
   })
 })
